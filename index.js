44f85d6f1ef3d6febca24c333a895bd4486223a0
Bruno Cardoso <brunocrpontes@gmail.com>
2023-01-20 03:01:59 -0300

    chore: having joy with recursive implementations of reduce, map, filter

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Maybe_1 = require("./source/Maybe");
-function getCurrentPosition() {
-    return (0, Maybe_1.Maybe)({ lat: 222, lng: 0.51923810283 });
+const sum = (a, b) => a + b;
+const double = (value) => value * 2;
+const isOdd = (value) => value % 2 === 0;
+function chunk(items, chunkSize, initial = []) {
+    const slices = items.length / chunkSize;
+    if (slices === 0)
+        return initial;
+    if (slices === 1)
+        return initial.concat([items]);
+    const head = items.slice(0, chunkSize);
+    const tail = items.slice(chunkSize);
+    return chunk(tail, chunkSize, initial.concat([head]));
 }
-getCurrentPosition()
-    .map((point) => {
-    console.log(point);
-    return (0, Maybe_1.Nothing)();
-})
-    .map(value => console.log("DID I RUN?", { value }));
+function map(element, transform, initial = []) {
+    return reduce(element, (current, value) => current.concat(transform(value)), initial);
+}
+function filter(element, predicate, initial = []) {
+    return reduce(element, (current, value) => {
+        if (predicate(value))
+            return current.concat(value);
+        return current;
+    }, initial);
+}
+function reduce(element, transform, initial) {
+    if (element.length < 1)
+        return initial;
+    const [head, ...tail] = element;
+    const next = transform(initial, head);
+    if (element.length === 1)
+        return next;
+    return reduce(tail, transform, next);
+}
+const array = Array.from({ length: 50 }, (_, index) => index + 1);
+const groups = chunk(array, 5);
+const doubled = map(array, double);
+const odd = filter(array, isOdd);
+const summation = reduce(array, sum, 0);
+const summationOfDoubled = reduce(map(map(groups, value => reduce(value, sum, 0)), double), sum, 0);
+console.log({
+    groups,
+    doubled,
+    odd,
+    summation,
+    summationOfDoubled
+});
--- index.ts
+++ index.ts
@@ -1,14 +1,64 @@
-import { Either } from "./source/Either"
-import { Maybe, Nothing } from "./source/Maybe"
+const sum = (a: number, b: number) => a + b
+const double = (value: number) => value * 2
+const isOdd = (value: number) => value % 2 === 0
 
-type Point = { lat: number, lng: number }
-function getCurrentPosition(): Maybe<Point> {
-    return Maybe({lat: 222, lng: 0.51923810283})
+function chunk<T>(items: T[], chunkSize: number, initial: T[][] = []): T[][] {
+    const slices = items.length / chunkSize
+
+    if (slices === 0) return initial;
+
+    if(slices === 1) return initial.concat([items])
+
+    const head = items.slice(0, chunkSize)
+    const tail = items.slice(chunkSize)
+
+    return chunk(tail, chunkSize, initial.concat([head]))
+}
+function map<T, B>(element: T[], transform: (value: T) => B, initial: B[] = []): B[] {
+    return reduce(
+        element,
+        (current, value) => current.concat(transform(value)),
+        initial
+    )
 }
 
-getCurrentPosition()
-    .map((point) => {
-        console.log(point);
-        return Nothing()
-    })
-    .map(value => console.log("DID I RUN?", { value }))
+function filter<T>(element: T[], predicate: (value: T) => boolean, initial: T[] = []): T[] {
+    return reduce(
+        element,
+        (current, value) => {
+            if(predicate(value)) return current.concat(value)
+            return current;
+        },
+        initial
+    )
+}
+
+function reduce<T, B>(element: T[], transform: (current: B, value: T) => B, initial: B): B {
+    if(element.length < 1) return initial;
+
+    const [head, ...tail] = element;
+    const next = transform(initial, head)
+
+    if(element.length === 1) return next
+
+    return reduce(
+        tail,
+        transform,
+        next
+    )
+}
+
+const array = Array.from({length: 50}, (_, index) => index + 1);
+
+const groups = chunk(array, 5)
+const doubled = map(array, double)
+const odd = filter(array, isOdd)
+const summation = reduce(array, sum, 0)
+
+
+console.log({
+    groups,
+    doubled,
+    odd,
+    summation,
+})
