591fb879a0e57c783f66290d009358c6b7bd36cb
Bruno Cardoso <brunocrpontes@gmail.com>
2023-01-20 03:18:24 -0300

    feat: create recursive some, every, flat and flatMap

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sum = (a, b) => a + b;
-const double = (value) => value * 2;
-const isOdd = (value) => value % 2 === 0;
 function chunk(items, chunkSize, initial = []) {
     const slices = items.length / chunkSize;
     if (slices === 0)
@@ -13,35 +10,58 @@ function chunk(items, chunkSize, initial = []) {
     const tail = items.slice(chunkSize);
     return chunk(tail, chunkSize, initial.concat([head]));
 }
-function map(element, transform, initial = []) {
-    return reduce(element, (current, value) => current.concat(transform(value)), initial);
+function map(items, transform, initial = []) {
+    return reduce(items, (current, value) => current.concat(transform(value)), initial);
 }
-function filter(element, predicate, initial = []) {
-    return reduce(element, (current, value) => {
+function every(items, predicate) {
+    return reduce(items, (current, value) => predicate(value), false);
+}
+function some(items, predicate) {
+    return reduce(items, (current, value) => predicate(value) || current, false);
+}
+function flat(items, initial = []) {
+    return reduce(items, (current, item) => current.concat(item), initial);
+}
+function flatMap(items, transform) {
+    return map(flat(items), transform);
+}
+function filter(items, predicate, initial = []) {
+    return reduce(items, (current, value) => {
         if (predicate(value))
             return current.concat(value);
         return current;
     }, initial);
 }
-function reduce(element, transform, initial) {
-    if (element.length < 1)
+function reduce(items, transform, initial) {
+    if (items.length < 1)
         return initial;
-    const [head, ...tail] = element;
+    const [head, ...tail] = items;
     const next = transform(initial, head);
-    if (element.length === 1)
+    if (items.length === 1)
         return next;
     return reduce(tail, transform, next);
 }
-const array = Array.from({ length: 50 }, (_, index) => index + 1);
+const sum = (a, b) => a + b;
+const double = (value) => value * 2;
+const isOdd = (value) => value % 2 === 0;
+const isGreaterThanZero = (value) => value > 0;
+const isGreatherThanFifty = (value) => value > 50;
+const array = Array.from({ length: 51 }, (_, index) => index + 1);
 const groups = chunk(array, 5);
+const flattedGroups = flat(groups);
+const flattedAndDoubled = flatMap(groups, double);
 const doubled = map(array, double);
 const odd = filter(array, isOdd);
+const isEveryValueIsGreatherThan0 = every(array, isGreaterThanZero);
+const hasSomeValueGreatherThan50 = some(array, isGreatherThanFifty);
 const summation = reduce(array, sum, 0);
-const summationOfDoubled = reduce(map(map(groups, value => reduce(value, sum, 0)), double), sum, 0);
 console.log({
     groups,
+    flattedGroups,
+    flattedAndDoubled,
     doubled,
     odd,
     summation,
-    summationOfDoubled
+    isEveryValueIsGreatherThan0,
+    hasSomeValueGreatherThan50
 });
--- index.ts
+++ index.ts
@@ -1,30 +1,56 @@
-const sum = (a: number, b: number) => a + b
-const double = (value: number) => value * 2
-const isOdd = (value: number) => value % 2 === 0
-
 function chunk<T>(items: T[], chunkSize: number, initial: T[][] = []): T[][] {
     const slices = items.length / chunkSize
 
     if (slices === 0) return initial;
 
-    if(slices === 1) return initial.concat([items])
+    if (slices === 1) return initial.concat([items])
 
     const head = items.slice(0, chunkSize)
     const tail = items.slice(chunkSize)
 
     return chunk(tail, chunkSize, initial.concat([head]))
 }
-function map<T, B>(element: T[], transform: (value: T) => B, initial: B[] = []): B[] {
+function map<T, B>(items: T[], transform: (value: T) => B, initial: B[] = []): B[] {
     return reduce(
-        element,
+        items,
         (current, value) => current.concat(transform(value)),
         initial
     )
 }
 
-function filter<T>(element: T[], predicate: (value: T) => boolean, initial: T[] = []): T[] {
+function every<T>(items: T[], predicate: (value: T) => boolean) {
     return reduce(
-        element,
+        items,
+        (current, value) => predicate(value),
+        false
+    )
+}
+function some<T>(items: T[], predicate: (value: T) => boolean) {
+    return reduce(
+        items,
+        (current, value) => predicate(value) || current,
+        false
+    )
+}
+
+function flat<T>(items: T[][], initial: T[] = []){
+    return reduce(
+        items,
+        (current, item) => current.concat(item),
+        initial
+    )
+}
+
+function flatMap<T, B>(items: T[][], transform: (value: T) => B): B[] {
+    return map(
+        flat(items),
+        transform,
+    )
+}
+
+function filter<T>(items: T[], predicate: (value: T) => boolean, initial: T[] = []): T[] {
+    return reduce(
+        items,
         (current, value) => {
             if(predicate(value)) return current.concat(value)
             return current;
@@ -33,13 +59,13 @@ function filter<T>(element: T[], predicate: (value: T) => boolean, initial: T[] 
     )
 }
 
-function reduce<T, B>(element: T[], transform: (current: B, value: T) => B, initial: B): B {
-    if(element.length < 1) return initial;
+function reduce<T, B>(items: T[], transform: (current: B, value: T) => B, initial: B): B {
+    if(items.length < 1) return initial;
 
-    const [head, ...tail] = element;
+    const [head, ...tail] = items;
     const next = transform(initial, head)
 
-    if(element.length === 1) return next
+    if(items.length === 1) return next
 
     return reduce(
         tail,
@@ -48,17 +74,32 @@ function reduce<T, B>(element: T[], transform: (current: B, value: T) => B, init
     )
 }
 
-const array = Array.from({length: 50}, (_, index) => index + 1);
+const sum = (a: number, b: number) => a + b;
+const double = (value: number) => value * 2;
+const isOdd = (value: number) => value % 2 === 0;
+const isGreaterThanZero = (value: number) => value > 0
+const isGreatherThanFifty = (value: number) => value > 50
+
+const array = Array.from({length: 51}, (_, index) => index + 1);
 
 const groups = chunk(array, 5)
+const flattedGroups = flat(groups)
+const flattedAndDoubled = flatMap(groups, double)
 const doubled = map(array, double)
 const odd = filter(array, isOdd)
+const isEveryValueIsGreatherThan0 = every(array, isGreaterThanZero)
+const hasSomeValueGreatherThan50 = some(array, isGreatherThanFifty)
+
 const summation = reduce(array, sum, 0)
 
 
 console.log({
     groups,
+    flattedGroups,
+    flattedAndDoubled,
     doubled,
     odd,
     summation,
+    isEveryValueIsGreatherThan0,
+    hasSomeValueGreatherThan50
 })
